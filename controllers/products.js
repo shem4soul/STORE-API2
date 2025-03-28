6c3c68af03a1c05d40469a67a689c0fc61390f73
shem4soul@gmail.com <shem4soul@gmail.com>
2025-03-28 14:42:09 +0100

    queryObject

--- controllers/products.js
+++ controllers/products.js
@@ -1,19 +1,42 @@
-// const getAllProductsStatic = async (req, res) => {
-//     throw new Error('testing async errors')
-//     res.status(200).json({msg: 'products testing routes'})
-// }
-const getAllProductsStatic = async (req, res, next) => {
-    try {
-      throw new Error('testing async errors'); // This will be caught
-    } catch (error) {
-      next(error); // Pass error to Express error handler
-    }
-  };
+const Product = require('../models/product')
+
+const getAllProductsStatic = async (req, res) => {
+  const products = await Product.find({
+    name: 'vase table',
+  })
+    res.status(200).json({products, nbHits: products.length})
+}
   
 
+
 const getAllProducts = async (req, res) => {
-    res.status(200).json({msg: 'products routes'})
+  const {featured, company } = req.query
+  const queryObject = {}
+
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true:false
+  }
+
+  if (company) {
+    queryObject.company = company
+  }
+  console.log(queryObject);
+  
+  const products = await Product.find(queryObject)
+  res.status(200).json({products, nbHits: products.length})
 }
+
+// // const getAllProducts = async (req, res) => {
+// //     try {
+// //         const products = await Product.find({});
+// //         res.status(200).json({ products, nbHits: products.length });
+// //     } catch (error) {
+// //         res.status(500).json({ message: 'Server error', error });
+// //     }
+// // };
+
+module.exports = { getAllProducts };
+
 
 
 module.exports = {

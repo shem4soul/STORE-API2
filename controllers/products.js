c971aec24d31f59e15431e1c61797a08b79c42fc
shem4soul@gmail.com <shem4soul@gmail.com>
2025-03-29 19:09:38 +0100

    pagination

--- controllers/products.js
+++ controllers/products.js
@@ -1,16 +1,16 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({
-    name: 'vase table',
-  })
+   const products = await Product.find({}).sort('name').select('name price')
+   .limit(10)
+   .skip(5)
     res.status(200).json({products, nbHits: products.length})
 }
   
 
 
 const getAllProducts = async (req, res) => {
-  const {featured, company } = req.query
+  const {featured, company, name, sort, fields } = req.query
   const queryObject = {}
 
   if (featured) {
@@ -20,20 +20,39 @@ const getAllProducts = async (req, res) => {
   if (company) {
     queryObject.company = company
   }
-  console.log(queryObject);
-  
-  const products = await Product.find(queryObject)
-  res.status(200).json({products, nbHits: products.length})
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i'}
+  }
+// console.log(queryObject)
+let  result = Product.find(queryObject)
+//
+if (sort) {
+  const sortList = sort.split(',').join(' ');
+  result = result.sort(sortList)
+}
+else{
+  result = result.sort('createdAt') 
 }
 
-// // const getAllProducts = async (req, res) => {
-// //     try {
-// //         const products = await Product.find({});
-// //         res.status(200).json({ products, nbHits: products.length });
-// //     } catch (error) {
-// //         res.status(500).json({ message: 'Server error', error });
-// //     }
-// // };
+if (fields) {
+  const fieldsList = fields.split(',').join(' ');
+  result = result.select(fieldsList)
+}
+
+ const page = Number(req.query.page) || 1
+const limit = Number(req.query.limit) || 10
+const skip = (page -1) * limit;
+
+result = result.skip(skip).limit(limit)
+//23
+//4 7 7 7 2
+
+
+
+    const products = await result
+  res.status(200).json({products, nbHits: products.length})
+}
 
 module.exports = { getAllProducts };
 
